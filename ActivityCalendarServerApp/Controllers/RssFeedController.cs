c73f4761acf367277b5492f03e8547200366efc5
HudiksvallsOK <hudiksvalls.ok.hemsida@gmail.com>
2022-08-12 22:23:51 +0200

    ActivityCalendarServerApp

---
+++ ActivityCalendarServerApp/Controllers/RssFeedController.cs
@@ -0,0 +1,69 @@
+﻿using ActivityCalendarServerApp.Pages;
+using ActivityCalendarCore.Data;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Mvc;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Xml;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ActivityCalendarServerApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RssFeedController : ControllerBase
+	{
+		private ActivityService ActivityService { get; }
+		public ILocalStorageService LocalStorageService { get; }
+		public IndexVm Vm { get; }
+
+		public RssFeedController(ILocalStorageService localStorageService)
+		{
+			this.ActivityService = new ActivityService();
+			this.LocalStorageService = localStorageService;
+			this.Vm = new IndexVm(this.LocalStorageService);
+		}
+
+		// GET: api/<RssFeedController>
+		[HttpGet(Name = "RSSFeed")]
+		public string Get()
+		{
+			var feed = new SyndicationFeed("Title", "Description", new Uri(HttpContext.Request.Host.ToUriComponent()), "HOKRssFeed1", DateTime.Now);
+
+			feed.Copyright = new TextSyndicationContent($"{DateTime.Now.Year} Bantarn");
+
+			var settings = new XmlWriterSettings
+			{
+				Encoding = Encoding.UTF8,
+				NewLineHandling = NewLineHandling.Entitize,
+				NewLineOnAttributes = false,
+				Indent = true,
+			};
+
+			var yearMonthActivities = this.ActivityService.GetActivitiesAsync(this.Vm.ActivityTypes.Where(x => x.IsIncluded).Select(x => x.ActivityType)
+				, this.Vm.DaysHorizon
+			);
+
+			var items = new List<SyndicationItem>();
+
+			foreach (var item in yearMonthActivities.Result.SelectMany(x => x))
+			{
+				items.Add(new SyndicationItem(item.Summary, item.Description, item.Uri, item.Id, item.CalendarDateTime));
+			}
+
+			feed.Items = items;
+
+			var sw = new StringWriter();
+
+			using (var writer = XmlWriter.Create(sw, settings))
+			{
+				var rssFormator = new Rss20FeedFormatter(feed, false);
+				rssFormator.WriteTo(writer);
+				writer.Flush();
+			}
+
+			return sw.ToString();
+		}
+	}
+}
---
+++ ActivityCalendarServerApp/Pages/Index.razor.cs
@@ -0,0 +1,77 @@
+﻿using ActivityCalendarCore.Core;
+
+namespace ActivityCalendarServerApp.Pages
+{
+	public partial class Index
+	{
+		public IndexVm? Vm { get; set; }
+
+		public Index()
+		{
+		}
+
+		private ILookup<string, Activity>? yearMonthActivities;
+
+		protected override async Task OnInitializedAsync()
+		{
+			this.Vm = new IndexVm(this.LocalStorageService);
+		}
+
+		protected override void OnInitialized()
+		{
+		}
+
+		protected override Task OnAfterRenderAsync(bool firstRender)
+		{
+			var task = base.OnAfterRenderAsync(firstRender);
+
+			if (firstRender)
+			{
+				return this.GetDefaults();
+			}
+
+			return task;
+		}
+
+		public void SelectAllOrNothing()
+		{
+			this.Vm?.SelectAllOrNothing();
+
+			this.StateHasChanged();
+		}
+
+		public async Task GetDefaults()
+		{
+			if (this.Vm != null)
+			{
+				await this.Vm.LoadDefaults();
+
+				await this.Filter();
+			}
+		}
+
+
+		private bool filtering;
+		public async Task Filter()
+		{
+			if (this.Vm != null)
+			{
+				this.filtering = true;
+
+				await Task.Delay(1);
+
+				this.yearMonthActivities = await this.ActivityService.GetActivitiesAsync(this.Vm.ActivityTypes.Where(x => x.IsIncluded).Select(x => x.ActivityType)
+					, this.Vm.DaysHorizon
+					);
+
+				this.filtering = false;
+
+				this.StateHasChanged();
+			}
+		}
+
+		public string LoaderVisibiblity => this.filtering
+			? "visible"
+			: "hidden";
+	}
+}
